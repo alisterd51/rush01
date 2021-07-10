2852c99d9f50c0cbc5ac536e2036876e89c454ef
Antoine CLARMAN <anclarma@e1r1p8.42.fr>
2021-07-10 22:11:11 +0200

    fonction de check en cours

--- bonus/check.c
+++ bonus/check.c
@@ -6,11 +6,11 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 18:15:47 by anclarma          #+#    #+#             */
-/*   Updated: 2021/07/10 21:41:12 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/07/10 22:10:59 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	count_skyscraper(const int *tab)
+static int	count_skyscraper(int line_len, int *tab)
 {
 	int	ret;
 	int	max;
@@ -19,7 +19,7 @@ static int	count_skyscraper(const int *tab)
 	ret = 0;
 	max = 0;
 	i = 0;
-	while (max < 4 && i < 4)
+	while (max < line_len && i < line_len)
 	{
 		if (tab[i] > max)
 		{
@@ -78,15 +78,25 @@ int	check_colup(int line_len, int colup, int *map, int x)
 
 int	check_coldown(int line_len, int coldown, int *map, int x)
 {
-	const int	tab[4] = {map[3 * 4 + x], map[2 * 4 + x],
-							map[1 * 4 + x], map[0 * 4 + x]};
-	int			count;
+	int	tab[9];
+	int	count;
+	int	i;
 
-	count = count_skyscraper(tab);
-	if ((coldown == 1 && tab[0] != 4)
-		|| (coldown == 2 && count != 2)
-		|| (coldown == 3 && count != 3)
-		|| (coldown == 4 && count != 4))
+	i = 0;
+	while (line_len - 1 - i >= 0)
+	{
+		tab[i] = map[(line_len - 1 - i) * line_len + x];
+		i++;
+	}
+	if (coldown == 1 && tab[0] != line_len)
 		return (1);
+	i = line_len;
+	count = count_skyscraper(line_len, tab);
+	while (i > 1)
+	{
+		if (coldown == i && count != i)
+			return (1);
+		i--;
+	}
 	return (0);
 }
